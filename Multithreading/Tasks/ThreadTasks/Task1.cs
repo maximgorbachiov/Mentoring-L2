7f22830225459c28049789ba381c14b19fa89896
Maksim.Harbachou <maksimka.gorbachev.95@gmail.com>
2019-03-02 15:34:35 +0300

    Task1

---
+++ Multithreading/Tasks/ThreadTasks/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThreadTasks
+{
+    public static class Task1
+    {
+        public static void RunTasks()
+        {
+            Task[] tasks = new Task[100];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int number = i + 1;
+
+                tasks[i] = Task.Factory.StartNew(() =>
+                {
+                    int iterationsCount = 1000;
+
+                    for (int j = 1; j <= iterationsCount; j++)
+                    {
+                        Console.WriteLine($"Task #{number} - {{{j}}}");
+                    }
+                });
+            }
+
+            Task.Factory.ContinueWhenAll(tasks, (completedTasks) => { Console.WriteLine("All tasks finished their work"); });
+        }
+    }
+}
---
+++ Multithreading/Tasks/UserConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreadTasks;
+
+namespace UserConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task1.RunTasks();
+            Console.ReadKey();
+        }
+    }
+}
