b5dbc34698020b10caa8beb161a45f9315d49814
Maksim Harbachou2 <maksimka.gorbachev.95@gmail.com>
2019-03-05 21:35:47 +0300

    Task7

---
+++ Multithreading/Tasks/ThreadTasks/Task7/CustomTaskScheduler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTasks.Task7
+{
+    public class CustomTaskScheduler : TaskScheduler
+    {
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return new List<Task>();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            Thread thread = new Thread(() =>
+            {
+                Console.WriteLine("Custom task scheduler run new thread not from pull");
+                Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId}");
+                base.TryExecuteTask(task);
+            });
+            thread.Start();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            return base.TryExecuteTask(task);
+        }
+    }
+}
--- Multithreading/Tasks/ThreadTasks/Task7/Task7.cs
+++ Multithreading/Tasks/ThreadTasks/Task7/Task7.cs
@@ -1,12 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadTasks.Task7
 {
     public static class Task7
     {
+        public static void RunScenarios()
+        {
+            RunTasks();
+        }
+
+        private static async void RunTasks()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken ct = cts.Token;
+            await RunTasks(1, ct);
+            await RunTasks(-1, ct);
+            RunTasks(1, ct);
+            cts.Cancel();
+        }
+
+        private static Task<TaskResults> RunTasks(int value, CancellationToken ct)
+        {
+            Task<TaskResults> task = Task.Factory.StartNew(() => RunParentTask(value, ct), ct);
+            task.ContinueWith((parentTask) =>
+            {
+                Console.WriteLine("This task executed regardless of the result of the parent task");
+                Console.WriteLine($"Runs on the thread: {Thread.CurrentThread.ManagedThreadId}");
+            });
+            task.ContinueWith((parentTask) =>
+            {
+                if (parentTask.IsFaulted)
+                {
+                    Console.WriteLine("This task executed when parent task failed");
+                    Console.WriteLine($"Error: {parentTask.Exception.Message}");
+                    Console.WriteLine($"Runs on the thread: {Thread.CurrentThread.ManagedThreadId}");
+                }
+                else
+                {
+                    Console.WriteLine("This task was cancelled");
+                    Console.WriteLine($"Runs on the thread: {Thread.CurrentThread.ManagedThreadId}");
+                }
+            }, TaskContinuationOptions.NotOnRanToCompletion);
+            task.ContinueWith((parentTask) =>
+            {
+                Console.WriteLine("This task executed synchronously when parent task failed");
+                Console.WriteLine($"Error: {parentTask.Exception.Message}");
+                Console.WriteLine($"Runs on the thread: {Thread.CurrentThread.ManagedThreadId}");
+            }, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+            task.ContinueWith((parentTask) =>
+            {
+                Console.WriteLine("This task should execute not in a thread pool");
+                Console.WriteLine($"Runs on the thread: {Thread.CurrentThread.ManagedThreadId}");
+            }, ct, TaskContinuationOptions.OnlyOnCanceled, new CustomTaskScheduler());
+
+            return task;
+        }
+
+        private static TaskResults RunParentTask(int value, CancellationToken cts)
+        {
+            Console.WriteLine($"Parent task for all other tasks");
+            Console.WriteLine($"Runs on the thread: {Thread.CurrentThread.ManagedThreadId}");
+            if (cts.IsCancellationRequested)
+            {
+                return TaskResults.Cancelled;
+            }
+            if (value < 0)
+            {
+                throw new ArgumentException("value should be more than 0 to be executed with success or equal to 0 to be executed without success and without failure");
+            }
+            return TaskResults.Success;
+        }
+    }
+
+    public enum TaskResults
+    {
+        Success,
+        Cancelled
     }
 }
