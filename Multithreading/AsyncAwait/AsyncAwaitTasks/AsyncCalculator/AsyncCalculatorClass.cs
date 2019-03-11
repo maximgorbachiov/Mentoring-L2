e40ab2df0e521a04855a72986793df6778ddc992
Maksim.Harbachou <maksimka.gorbachev.95@gmail.com>
2019-03-11 23:30:05 +0300

    Async await task1 added

---
+++ Multithreading/AsyncAwait/AsyncAwaitTasks/AsyncCalculator/AsyncCalculatorClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncCalculator
+{
+    public static class AsyncCalculatorClass
+    {
+        public static void UserProcessStart()
+        {
+            int userNumber;
+            CancellationTokenSource cts = null;
+
+            Console.WriteLine("Enter value more than 0 to start calculation or equal or less than 0 to finish calculations at all");
+            do
+            {
+                string n = Console.ReadLine();
+
+                while (!int.TryParse(n, out userNumber))
+                {
+                    Console.WriteLine("Value should be a number type!");
+                    n = Console.ReadLine();
+                }
+
+                if (cts != null)
+                {
+                    cts.Cancel();
+                }
+
+                if (userNumber > 0)
+                {
+                    cts = new CancellationTokenSource();
+                    CalculateSum(userNumber, cts.Token);
+                    Console.WriteLine("Calculation started. Enter another value more than 0 to finish current calculation and start new one");
+                }
+            }
+            while (userNumber > 0);
+
+            Console.WriteLine("Calculation finished");
+        }
+
+        private static async Task CalculateSum(int userNumber, CancellationToken ct)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                int temp = 0;
+
+                for (int i = 0; i < userNumber; i++)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        return;
+                    }
+                    Thread.Sleep(1);
+                    temp += i;
+                }
+                Console.WriteLine($"Sum of all elements from 0 to {userNumber} = {temp}");
+            }, ct);
+        }
+    }
+}
---
+++ Multithreading/AsyncAwait/AsyncAwaitTasks/UserUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AsyncCalculator;
+
+namespace UserUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Task1 started");
+            AsyncCalculatorClass.UserProcessStart();
+            Console.WriteLine("Task1 ended");
+            Console.ReadLine();
+        }
+    }
+}
